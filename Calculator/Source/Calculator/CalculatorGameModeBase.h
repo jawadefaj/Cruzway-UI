6888b451dfbed242da6938f7cb3114643bacec30
MaggiePeterson <margaret.peterson@gmail.com>
2020-07-01 10:02:28 -0700

    added buttons

---
+++ Calculator/Source/Calculator/CalculatorGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CalculatorGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CALCULATOR_API ACalculatorGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
