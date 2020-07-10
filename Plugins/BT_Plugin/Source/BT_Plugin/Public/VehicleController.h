60560cee4890b824d1c849014d929beaa4c1d524
jawadefaj <jawadefaj006@gmail.com>
2020-07-10 15:32:49 -0700

    BT_Plugin

---
+++ Plugins/BT_Plugin/Source/BT_Plugin/Public/VehicleController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "VehicleController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BT_PLUGIN_API AVehicleController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
